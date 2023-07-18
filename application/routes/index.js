85bcffb533ae216fbb8efe84436c43c49b476a56
Juan <juan5801331@gmail.com>
2023-07-18 16:01:44 -0700

    Milestone 1(I forgot to push it =(

--- application/routes/index.js
+++ application/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+
+
+
 /* GET home page. */
+//localhost:300/users/
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CSC 317 App', name:"[Insert your name here]" });
+  res.render('index',{title:'CSC 317 APP', name: "Juan Jose Estrada"});
+});
+
+router.get("/login",function(re,res,next){
+    res.removeHeader('login');
 });
 
 module.exports = router;
--- application/routes/users.js
+++ application/routes/users.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+router.use(function(re,res,next){
+  console.log('Users middleware1-- users.js');
+  next()
+})
+
+router.use(function(re,res,next){
+  console.log('Users middleware2-- users.js');
+  next()
+})
+
 /* GET users listing. */
+/* GET home page. */
+//localhost:300/users/
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('respond with a source from get');
+});
+//localhost:300/users/
+/*router.update('/', function(req, res, next) {
+  res.render('respond with a source from get');
+});*/
+//localhost:300/users/
+router.post('/', function(req, res, next) {
+  res.render('respond with a source from post');
+});
+//localhost:300/users/
+router.patch('/', function(req, res, next) {
+  res.render('respond with a source from patch');
 });
 
+router.delete('/', function(req, res, next) {
+  res.render('respond with a source from delete');
+});
 module.exports = router;
