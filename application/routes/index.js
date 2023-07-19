d10c04db5883ee45b5546664ecb6214f8d31c85e
Juan <juan5801331@gmail.com>
2023-07-18 17:53:11 -0700

    milestone2

--- application/routes/index.js
+++ application/routes/index.js
@@ -7,11 +7,24 @@ var router = express.Router();
 /* GET home page. */
 //localhost:300/users/
 router.get('/', function(req, res, next) {
-  res.render('index',{title:'CSC 317 APP', name: "Juan Jose Estrada"});
+  res.render('index',{title:"Home",css :["index.css"],js:["index.js"]});
 });
 
 router.get("/login",function(re,res,next){
-    res.removeHeader('login');
+  res.render('login',{title:"Login",css :["login.css"]});
 });
+
+router.get("/registration",function(re,res,next){
+    res.render('registration',{title:"Registration",css :["registration.css"], js:["registration.js"]})
+});
+
+router.get("/postvideo",function(re,res,next){
+  res.render('postvideo',{title:"PostVideo",css :["postVideo.css"]});
+});
+
+router.get("/viewpost/",function(re,res,next){
+  res.render('viewpost',{title:"Viewpost",css :["viewpost.css"]});
+});
+
 
 module.exports = router;
